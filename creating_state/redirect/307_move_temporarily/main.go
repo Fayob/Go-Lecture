dfc8c1b318047c09f08072d096e64a747bd8857b
fayob <adedejifavour6@gmail.com>
2023-05-19 18:25:01 +0100

    worked with 307 move temporarily redirect

---
+++ creating_state/redirect/307_move_temporarily/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+var tpl *template.Template
+
+func init() {
+	fileDir, err := filepath.Abs("creating_state/redirect/307_move_temporarily")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	tpl = template.Must(template.ParseGlob(fileDir + "/*"))
+}
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/bar", bar)
+	http.HandleFunc("/barred", barred)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	fmt.Print("Your request method at foo: ", req.Method, "\n\n")
+}
+
+func bar(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Your request method at bar:", req.Method)
+	// process form submission here
+	http.Redirect(w, req, "/", http.StatusTemporaryRedirect)
+}
+
+func barred(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Your request method at barred:", req.Method)
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
